9d6a1370e70b32f640066836d61f702d22292403
Teedex1 <opeyemiridwan8965@gmail.com>
2024-09-06 13:11:29 +0100

    Assignment

---
+++ adder.py
@@ -0,0 +1,17 @@
+def Addition():
+    num1 = int(input("Input the first number"))
+    num2 = int(input ("Input the second number"))
+
+    result = num1 + num2
+    print("The sum is:", result)
+
+Addition()
+
+def Subtraction():
+    num1 = int(input("Input the first number"))
+    num2 = int(input ("Input the second number"))
+
+    result = num1 - num2
+    print("The sum is:", result)
+
+Subtraction()
