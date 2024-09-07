11588f5a02ea135d262824c526c98a5fdc91b516
Teedex1 <opeyemiridwan8965@gmail.com>
2024-09-07 20:56:21 +0100

    assignment

--- adder.py
+++ adder.py
@@ -1,6 +1,6 @@
 def Addition():
-    num1 = int(input("Input the first number"))
-    num2 = int(input ("Input the second number"))
+    num1 = int(input("Input the first number: "))
+    num2 = int(input("Input the second number: "))
 
     result = num1 + num2
     print("The sum is:", result)
@@ -8,10 +8,29 @@ def Addition():
 Addition()
 
 def Subtraction():
-    num1 = int(input("Input the first number"))
-    num2 = int(input ("Input the second number"))
+    num1 = int(input("Input the first number: "))
+    num2 = int(input("Input the second number: "))
 
     result = num1 - num2
-    print("The sum is:", result)
+    print("The difference is:", result)
 
 Subtraction()
+
+def Multiplication():
+    num1 = int(input("Input the first number: "))
+    num2 = int(input("Input the second number: "))
+
+    result = num1 * num2
+    print("The Answer is:", result)
+
+Multiplication()
+
+def Division():
+    num1 = int(input("Input the first number: "))
+    num2 = int(input("Input the second number: "))
+
+    if num2 != 0:
+        result = num1 / num2
+        print("The quotient is:", result)
+    
+Division()
